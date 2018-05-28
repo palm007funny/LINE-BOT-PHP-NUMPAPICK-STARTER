5db3aa745971f1ddd0ead3317af9ffd92425986a
Thanawat Inkeaw <thanawat.inkeaw@gmail.com>
2018-05-28 16:31:56 +0700

    Update line.php

--- line.php
+++ line.php
@@ -11,7 +11,16 @@ function send_LINE($msg){
       ];
 
       // Make a POST Request to Messaging API to reply to sender
- 
+      if ($messages == "test") {   
+        "message": {
+        "id": "325708",
+        "type": "location",
+        "title": "my location",
+        "address": "〒150-0002 東京都渋谷区渋谷２丁目２１−１",
+        "latitude": 35.65910807942215,
+        "longitude": 139.70372892916203
+        }
+      }
       $url = 'https://api.line.me/v2/bot/message/push';
       $data = [
 
